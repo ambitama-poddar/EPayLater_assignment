1af723fc828e326606c48d36cbc1e13272ebfcd8
Ambitama Poddar <115911065+Ambitama01@users.noreply.github.com>
2023-07-09 17:59:40 +0530

    Added bonus point features

--- Task2/app1.py
+++ Task2/app1.py
@@ -1,17 +1,19 @@
-#Approach 1: SQLAlchemy
-
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import func
+from sqlalchemy.exc import IntegrityError
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:ambitama01@localhost:5432/housing_db'
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = "postgresql://postgres:ambitama01@localhost:5432/housing_db"
 db = SQLAlchemy(app)
 
-class house(db.Model):
+
+class House(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     bedrooms = db.Column(db.Integer)
-    bathrooms = db.Column(db.Float(precision='double'))
+    bathrooms = db.Column(db.Float(precision="double"))
     square_footage = db.Column(db.Integer)
     location = db.Column(db.String)
     sale_price = db.Column(db.Integer)
@@ -24,44 +26,75 @@ class house(db.Model):
         self.location = location
         self.sale_price = sale_price
 
-@app.route('/housing_data', methods=['POST'])
+
+@app.route("/housing_data", methods=["POST"])
 def create_housing_data():
-    data = request.json['data']
-    j=1
-    for i in data:
-        id=j
-        j=j+1
-        bedrooms = i['Bedrooms']
-        bathrooms = i['Bathrooms']
-        square_footage = i['SquareFootage']
-        location = i['Location']
-        sale_price = i['SalePrice']
-        new_data = house(id=id,bedrooms=bedrooms, bathrooms=bathrooms, square_footage=square_footage, location=location, sale_price=sale_price) 
-        db.session.add(new_data)
-    db.session.commit()
+    try:
+        data = request.json["data"]
+        j=1
+        for i in data:
+            id=j
+            j=j+1
+            bedrooms = i['Bedrooms']
+            bathrooms = i['Bathrooms']
+            square_footage = i['SquareFootage']
+            location = i['Location']
+            sale_price = i['SalePrice']
+            new_data = House(id=id,bedrooms=bedrooms, bathrooms=bathrooms, square_footage=square_footage, location=location, sale_price=sale_price) 
+            db.session.add(new_data)
+        db.session.commit()
+        return jsonify({"message": "Housing data created successfully"}), 201
+    except KeyError:
+        return jsonify({"error": "Invalid data format"}), 400
+    except IntegrityError:
+        db.session.rollback()
+        return jsonify({"error": "Duplicate entry or invalid data"}), 400
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
-    return jsonify({'message': 'Housing data created successfully'}), 201
 
-@app.route('/average_sale_price', methods=['GET'])
+@app.route("/average_sale_price", methods=["GET"])
 def get_average_sale_price():
-    result = db.session.query(func.avg(house.sale_price)).scalar()
-    return jsonify({'average_sale_price': result})
+    try:
+        result = db.session.query(func.avg(House.sale_price)).scalar()
+        return jsonify({"average_sale_price": result}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
-@app.route('/average_sale_price_per_location', methods=['GET'])
+
+@app.route("/average_sale_price_per_location", methods=["GET"])
 def get_average_sale_price_per_location():
-    result = db.session.query(house.location, func.avg(house.sale_price)).group_by(house.location).all()
-    average_sale_prices = {location: avg_sale_price for location, avg_sale_price in result}
-    return jsonify({'average_sale_price_per_location': average_sale_prices})
+    try:
+        result = (
+            db.session.query(House.location, func.avg(House.sale_price))
+            .group_by(House.location)
+            .all()
+        )
+        average_sale_prices = {
+            location: avg_sale_price for location, avg_sale_price in result
+        }
+        return jsonify({"average_sale_price_per_location": average_sale_prices}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
-@app.route('/max_sale_price', methods=['GET'])
+
+@app.route("/max_sale_price", methods=["GET"])
 def get_max_sale_price():
-    result = db.session.query(func.max(house.sale_price)).scalar()
-    return jsonify({'max_sale_price': result})
+    try:
+        result = db.session.query(func.max(House.sale_price)).scalar()
+        return jsonify({"max_sale_price": result}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
-@app.route('/min_sale_price', methods=['GET'])
+
+@app.route("/min_sale_price", methods=["GET"])
 def get_min_sale_price():
-    result = db.session.query(func.min(house.sale_price)).scalar()
-    return jsonify({'min_sale_price': result})
+    try:
+        result = db.session.query(func.min(House.sale_price)).scalar()
+        return jsonify({"min_sale_price": result}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
--- Task2/app2.py
+++ Task2/app2.py
@@ -1,82 +1,106 @@
-#Approach 2: psycopg2 + SQL Queries
-
 from flask import Flask, jsonify, request
 import psycopg2
 from psycopg2.extras import RealDictCursor
 
 app = Flask(__name__)
 
+
 def connect_to_db():
     conn = psycopg2.connect(
-        host="localhost",
-        database="housing_db",
-        user="postgres",
-        password="ambitama01"
+        host="localhost", database="housing_db", user="postgres", password="ambitama01"
     )
     return conn
 
-@app.route('/housing_data', methods=['POST'])
+
+@app.route("/housing_data", methods=["POST"])
 def create_housing_data():
-    data = request.json['data']
-    conn = connect_to_db()
-    cur = conn.cursor(cursor_factory=RealDictCursor)
-    j = 1
-    for i in data:
-        id = j
-        j += 1
-        bedrooms = i['Bedrooms']
-        bathrooms = i['Bathrooms']
-        square_footage = i['SquareFootage']
-        location = i['Location']
-        sale_price = i['SalePrice']
-        cur.execute("INSERT INTO house (id, bedrooms, bathrooms, square_footage, location, sale_price) VALUES (%s, %s, %s, %s, %s, %s)",
-                    (id, bedrooms, bathrooms, square_footage, location, sale_price))
-    conn.commit()
-    cur.close()
-    conn.close()
+    try:
+        data = request.json["data"]
+        conn = connect_to_db()
+        cur = conn.cursor(cursor_factory=RealDictCursor)
+        j = 1
+        for i in data:
+            id = j
+            j += 1
+            bedrooms = i.get("Bedrooms")
+            bathrooms = i.get("Bathrooms")
+            square_footage = i.get("SquareFootage")
+            location = i.get("Location")
+            sale_price = i.get("SalePrice")
+            cur.execute(
+                "INSERT INTO house (id, bedrooms, bathrooms, square_footage, location, sale_price) VALUES (%s, %s, %s, %s, %s, %s)",
+                (id, bedrooms, bathrooms, square_footage, location, sale_price),
+            )
+        conn.commit()
+        cur.close()
+        conn.close()
+        return jsonify({"message": "Housing data created successfully"}), 201
+    except KeyError:
+        return jsonify({"error": "Invalid data format"}), 400
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
-    return jsonify({'message': 'Housing data created successfully'}), 201
 
-@app.route('/average_sale_price', methods=['GET'])
+@app.route("/average_sale_price", methods=["GET"])
 def get_average_sale_price():
-    conn = connect_to_db()
-    cur = conn.cursor()
-    cur.execute("SELECT AVG(sale_price) FROM house")
-    result = cur.fetchone()[0]
-    cur.close()
-    conn.close()
-    return jsonify({'average_sale_price': result})
+    try:
+        conn = connect_to_db()
+        cur = conn.cursor()
+        cur.execute("SELECT AVG(sale_price) FROM house")
+        result = cur.fetchone()[0]
+        cur.close()
+        conn.close()
+        return jsonify({"average_sale_price": result}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
-@app.route('/average_sale_price_per_location', methods=['GET'])
+
+@app.route("/average_sale_price_per_location", methods=["GET"])
 def get_average_sale_price_per_location():
-    conn = connect_to_db()
-    cur = conn.cursor(cursor_factory=RealDictCursor)
-    cur.execute("SELECT location, AVG(sale_price) as average_sale_price FROM house GROUP BY location")
-    result = cur.fetchall()
-    cur.close()
-    conn.close()
-    average_sale_prices = {row['location']: row['average_sale_price'] for row in result}
-    return jsonify({'average_sale_price_per_location': average_sale_prices})
+    try:
+        conn = connect_to_db()
+        cur = conn.cursor(cursor_factory=RealDictCursor)
+        cur.execute(
+            "SELECT location, AVG(sale_price) as average_sale_price FROM house GROUP BY location"
+        )
+        result = cur.fetchall()
+        cur.close()
+        conn.close()
+        average_sale_prices = {
+            row["location"]: row["average_sale_price"] for row in result
+        }
+        return jsonify({"average_sale_price_per_location": average_sale_prices}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
-@app.route('/max_sale_price', methods=['GET'])
+
+@app.route("/max_sale_price", methods=["GET"])
 def get_max_sale_price():
-    conn = connect_to_db()
-    cur = conn.cursor()
-    cur.execute("SELECT MAX(sale_price) FROM house")
-    result = cur.fetchone()[0]
-    cur.close()
-    conn.close()
-    return jsonify({'max_sale_price': result})
+    try:
+        conn = connect_to_db()
+        cur = conn.cursor()
+        cur.execute("SELECT MAX(sale_price) FROM house")
+        result = cur.fetchone()[0]
+        cur.close()
+        conn.close()
+        return jsonify({"max_sale_price": result}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
-@app.route('/min_sale_price', methods=['GET'])
+
+@app.route("/min_sale_price", methods=["GET"])
 def get_min_sale_price():
-    conn = connect_to_db()
-    cur = conn.cursor()
-    cur.execute("SELECT MIN(sale_price) FROM house")
-    result = cur.fetchone()[0]
-    cur.close()
-    conn.close()
-    return jsonify({'min_sale_price': result})
+    try:
+        conn = connect_to_db()
+        cur = conn.cursor()
+        cur.execute("SELECT MIN(sale_price) FROM house")
+        result = cur.fetchone()[0]
+        cur.close()
+        conn.close()
+        return jsonify({"min_sale_price": result}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
